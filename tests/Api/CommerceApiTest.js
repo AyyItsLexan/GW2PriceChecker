4f99068d80bbc5ab753d7131026ec79b8600f99e
A1asd <iamhldisch@hotmail.de>
2021-06-15 21:59:55 +0200

    moved test.js into tests/Api/CommerceApiTests.js
    support for multiple test files in the same way as directories are in src directory

--- tests/Api/CommerceApiTest.js
+++ tests/Api/CommerceApiTest.js
@@ -1,12 +1,7 @@
 const test = require('ava');
-const CommerceApi = require('./src/Api/CommerceApi.js');
+const CommerceApi = require('../../src/Api/CommerceApi.js');
 
 test('CommerceApi connects with service', async t => {
 	const commerceApi = new CommerceApi(200).getData();
 	t.is(await commerceApi.then(data => {return data.id}), 200);
-});
-
-test('bar', async t => {
-	const bar = Promise.resolve('bar');
-	t.is(await bar, 'bar');
 });
